262be22086f5f4df6337492f922e368961d56494
LUZ <arriolaluz77@gmail.com>
2024-05-29 17:55:02 -0300

    0.1

---
+++ script.js
@@ -0,0 +1,32 @@
+var estaciones= prompt("Ingrese una estacion en minuscula: "); 
+
+
+if(estaciones=="invierno"){
+
+
+alert("Usted eligio la estacion "+ estaciones + "\nInvierno: Inicia el 21 de junio y finaliza el 22 de septiembre.");
+
+}else if(estaciones=="primavera"){
+
+alert("Usted eligio la estacion "+ estaciones + "\nPrimavera: Inicia el 22 de septiembre y finaliza el 21 de diciembre. ");
+
+
+
+}else if(estaciones == "verano"){
+
+
+alert("Usted eligio la estacion " + estaciones + "\nVerano: Inicia el 21 de diciembre y finaliza el 21 de marzo.");
+
+
+}else if(estaciones =="otoño"){
+
+alert("Usted eligio la estacion " + estaciones + "\nOtoño: Inicia el 21 de marzo y finaliza el 21 de junio"); 
+
+
+}else{
+
+alert("¡ERROR! \nNo es una estacion");
+
+}
+
+
